262e50756976ed5b6cddd2c31559332e99153ae7
ilikespraying <2alex0607@gazeta.pl>
2026-01-15 14:32:08 +0100

    Moj trzeci fajniejszy commit :)

--- main.js
+++ main.js
@@ -3,3 +3,21 @@ document.querySelectorAll(".card").forEach(card => {
         window.location.href = 'https://youtu.be/dQw4w9WgXcQ?si=Gm44VzvSE0-hn0s9'
     });
 });
+
+const root = document.documentElement;
+let darkmode = true;
+
+document.querySelectorAll(".theme").forEach(card => {
+    card.addEventListener("click", () => {
+        if(darkmode){
+            root.style.setProperty("--bg-color"," #a7a7a8");
+            root.style.setProperty("--primary","#2e2e2e");
+            darkmode = false;
+        }else{
+            root.style.setProperty("--bg-color"," #0f172a");
+            root.style.setProperty("--primary"," #646464");
+            darkmode = true;
+        }
+        
+    });
+});
