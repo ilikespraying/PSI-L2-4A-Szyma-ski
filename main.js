21ba74648824b1b7ea3c3a220c6181b4b166c5ac
ilikespraying <2alex0607@gazeta.pl>
2026-01-15 14:06:30 +0100

    Moj drugi fajny commit :)

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelectorAll(".card").forEach(card => {
+    card.addEventListener("click", () => {
+        window.location.href = 'https://youtu.be/dQw4w9WgXcQ?si=Gm44VzvSE0-hn0s9'
+    });
+});
